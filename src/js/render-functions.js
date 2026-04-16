948dbab4af9b8673671ae7c4a89680f31469785a
vladatsapchuk <vladatsapchuk@gmail.com>
2026-04-16 17:48:20 +0300

    solveddd

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,27 +1,57 @@
-export const createGalleryCardTemplate = imgInfo => {
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-  return `
-    <li class="gallery-card">
-    <a class = "js-gallery-link" href = "${imgInfo.largeImageURL}"> <img class="gallery-img" src="${imgInfo.webformatURL}" alt="${imgInfo.tags}"/></a>
-    <div class="stats-container">
-        <ul class="stats-list">
-            <li class="gallery-item">
-                <span class="label">Likes</span>
-                <span class="value">${imgInfo.likes}</span>
-            </li>
-            <li class="gallery-item">
-                <span class="label">Views</span>
-                <span class="value">${imgInfo.views}</span>
-            </li>
-            <li class="gallery-item">
-                <span class="label">Comments</span>
-                <span class="value">${imgInfo.comments}</span>
-            </li>
-            <li class="gallery-item">
-                <span class="label">Downloads</span>
-                <span class="value">${imgInfo.downloads}</span>
-            </li>
-        </ul>
-    </div>
-</li>`
-};
+const galleryEl = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${webformatURL}"
+            alt="${tags}"
+          />
+        </a>
+        <div class="info">
+          <p class="info-item"><b>Likes</b> ${likes}</p>
+          <p class="info-item"><b>Views</b> ${views}</p>
+          <p class="info-item"><b>Comments</b> ${comments}</p>
+          <p class="info-item"><b>Downloads</b> ${downloads}</p>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+export function showLoader() {
+  loaderEl.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  loaderEl.classList.add('hidden');
+}
